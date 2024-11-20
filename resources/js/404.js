e26def9b1a6269c2bd4affc1773543d4134643ba
Hedreon <39467046+Hedreon@users.noreply.github.com>
2024-11-20 00:08:57 +0000

    Finalize 404 Page

---
+++ resources/js/404.js
@@ -0,0 +1,13 @@
+const DynamicElements = document.getElementsByClassName("dynamic")
+
+const Titles = [
+    "Hedreon is offline",
+    "Hedreon is not found",
+    "Clippy cannot be found",
+    `Hedreon: ${location.href.split("/").slice(-1)}`
+]
+
+window.addEventListener("load", function() {
+    document.title = Titles[Math.floor(Math.random() * Titles.length)];
+    DynamicElements[0].innerHTML = document.title;
+})

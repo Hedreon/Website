238819d910c07dce855b11526126a672422d0bff
Hedreon <39467046+Hedreon@users.noreply.github.com>
2025-03-23 01:13:17 +0000

    Fix small bug and introduce SEO

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Hedreon's personal website.">
+    <meta name="keywords" content="personal websites">
     <title>Hedreon: Home</title>
     <link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico">
     <link rel="stylesheet" href="/assets/css/global.css">

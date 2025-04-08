4ac0c6f2661fc33603211c0246a56d26a3560dbe
Hedreon <39467046+Hedreon@users.noreply.github.com>
2025-04-08 19:01:32 +0100

    Favicon linking is not needed anymore
    It is now set up in the web server.

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Hedreon's personal website.">
-    <meta name="keywords" content="hedreon, personal websites">
+    <meta name="keywords" content="hedreon">
     <title>Hedreon: Home</title>
-    <link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico">
     <link rel="stylesheet" href="/assets/css/global.css">
     <link rel="stylesheet" href="/assets/css/index.css">
 </head>
